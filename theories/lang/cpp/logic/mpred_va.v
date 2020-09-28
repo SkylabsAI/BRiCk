361d06213a40a3ea7fef100588aa940e5c9ce2fe
Hai Dang <hai@bedrocksystems.com>
2020-09-28 23:23:55 +0200

    add definition of vbyte

--- theories/lang/cpp/logic/mpred_va.v
+++ theories/lang/cpp/logic/mpred_va.v
@@ -14,18 +14,18 @@ Set Suggest Proof Using.
 Definition paddr := N.
 Definition vaddr := N.
 Definition byte := N.
+Definition pdname := N.
 
+Definition ASID : biIndex := BiIndex pdname _ eq _.
 
-Module Type CPP_PHYSICAL.
-  Context `{cpp_logic}.
+(** We are fixing that thread_info has 3 components, and the first component is
+  ASID. The other 2 components (K and H) remain abstract in this file. They will
+  be instantiated as the view for cache-effects of weak memory, and the view for
+  TLBs from cache-effects of the address translation. *)
 
-  Parameter byte_at : forall (hpa:paddr) (hpaFrac : Qp) (value : byte), mpred.
+Module CPP_PHYSICAL.
 
-End CPP_PHYSICAL.
-
-Definition ASID : biIndex := BiIndex vaddr _ eq _.
-
-Section asid_bi.
+Section with_cpp.
   Context {K H : biIndex}.
 
   Notation thread_info := (ASID *i K *i H).
@@ -34,18 +34,63 @@ Section asid_bi.
 
   Context `{Σ : cpp_logic thread_info}.
 
-  Definition vbyte' (va : vaddr) (v: byte) : monPred ASID (iPropI Σ) :=
-    MonPred (fun asid => emp)%I _.
-  Definition vbyte (va : vaddr) (v: byte) : mpred :=
-    monPred2_embed L_ASID (vbyte' va v).
+  Parameter byte_at : forall (pa:paddr) (q : Qp) (v : byte), mpred.
 
-  Lemma vbyte_objective_with_other va v : ObjectiveWith L_OTHER (vbyte va v).
+  (* Non-TLB assumption. When TLBs come into play, [page_mappped] will depend on
+    the TLB state. *)
+  Parameter page_mapped :
+    forall (γ : pdname) (va : vaddr) (q : Qp) (pa : paddr), (iPropI Σ).
+
+  (* Physical bytes should not depend on the address space *)
+  Axiom byte_at_objective_with_asid :
+    forall pa q v, ObjectiveWith L_ASID (byte_at pa q v).
+
+  (* SC assumption. In weak memory, [byte_at] will NOT be objective w.r.t thread
+    views. *)
+  Axiom byte_at_objective_with_other :
+    forall pa q v, ObjectiveWith L_OTHER (byte_at pa q v).
+
+  Existing Instances byte_at_objective_with_asid
+                     byte_at_objective_with_other.
+End with_cpp.
+End CPP_PHYSICAL.
+
+Module ASID_THEORY.
+Include CPP_PHYSICAL.
+Section with_cpp.
+  Context {K H : biIndex}.
+
+  Notation thread_info := (ASID *i K *i H).
+  Notation L_ASID := (Lid.l : MLens thread_info ASID).
+  Notation L_OTHER := (Lid.r : MLens thread_info (K *i H)).
+
+  Context `{Σ : cpp_logic thread_info}.
+
+  Implicit Types (q : Qp) (va : vaddr) (pa : paddr).
+  Definition page_mapping' va q pa : monPred ASID (iPropI Σ) :=
+    @MonPred ASID _ (fun γ => page_mapped γ va q pa)%I _.
+
+  Definition page_mapping va q pa : mpred :=
+    monPred2_embed L_ASID (page_mapping' va q pa).
+
+  Instance page_mapping_objective_with_other va q pa :
+    ObjectiveWith L_OTHER (page_mapping va q pa).
   Proof. apply monPred2_embed_objective_with_right. Qed.
 
-  Instance : Objective (@(L_ASID, asid) (vbyte va v)).
+  Instance page_mapping_exactly_with_objective_with :
+    Objective (@(L_ASID, asid) (page_mapping va q pa)).
   Proof.
-    intros. apply monPred_objective_with_id_objective.
+    intros.
+    apply monPred_objective_with_id_objective.
     apply monPred_exactly_with_objective_with_left.
-    apply vbyte_objective_with_other.
+    apply _.
   Qed.
-End asid_bi.
+
+  Definition vbyte va (v: byte) q : mpred :=
+    Exists pa, byte_at pa q v ** page_mapping va q pa.
+
+  Instance vbyte_objective_with_other va v q :
+    ObjectiveWith L_OTHER (vbyte va v q).
+  Abort.
+End with_cpp.
+End ASID_THEORY.
