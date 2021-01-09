19b74a0b1abeedb5f71961286bbc8a5640694616
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-01-09 21:56:47 +0100

    own_unit: Use type ascription, not cast (unlike Iris)
    Casts can block rewriting.

--- theories/lang/bi/own.v
+++ theories/lang/bi/own.v
@@ -88,7 +88,7 @@ Arguments HasOwnUpd _ {_} _ {_}.
 
 (* Separate from [HasOwnUpd] because this is restricted to unital cameras [ucmraT]. *)
 Class HasOwnUnit `{!BiBUpd PROP} {A : ucmraT} `{!HasOwn PROP A} : Type := {
-  own_unit γ : ⊢ |==> own γ (ε : A)
+  own_unit γ : ⊢ |==> own γ (ε (A := A))
 }.
 Arguments HasOwnUnit _ {_} _ {_}.
 
