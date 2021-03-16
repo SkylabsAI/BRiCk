7917e60e1d720db4cd8eaaa3f79fe2c656f3a28d
Gregory Malecha <gmalecha@gmail.com>
2021-03-15 21:12:00 -0400

    Initial Nix config for clang 11 (from Gregory)

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ nixpkgs ? import <nixpkgs> {  } }:
+
+let
+  pkgs = [
+    nixpkgs.clang_11
+    nixpkgs.llvm_11
+    nixpkgs.cmake
+  ];
+
+in
+  nixpkgs.stdenv.mkDerivation {
+    name = "env";
+    buildInputs = pkgs;
+    shellHook=''
+      export LLVM_DIR=${nixpkgs.llvm_11.outPath}
+      export CLANG_DIR=${nixpkgs.llvmPackages_11.clang-unwrapped.outPath}
+      '';
+  }
