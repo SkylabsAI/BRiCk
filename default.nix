ac7bcab0b4870d2d26e4921bc80e5e790d6cac42
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-04-20 15:23:48 +0200

    Pin a specific Nixpkgs version

--- default.nix
+++ default.nix
@@ -1,4 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {  } }:
+# Pin a specific version of the Nixpkgs repository; following
+# https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+{ nixpkgs ? import (builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/7919518f0235106d050c77837df5e338fb94de5d.tar.gz) {} }:
+
+# Longer-form version
+#{ nixpkgs ? import (builtins.fetchTarball {
+#  # Descriptive name to make the store path easier to identify
+#  name = "nixpkgs-unstable-2021-04-20";
+#  # Commit hash for nixos-unstable as of 2021-04-20
+#  url = "https://github.com/nixos/nixpkgs/archive/7919518f0235106d050c77837df5e338fb94de5d.tar.gz";
+#  # Hash obtained using `nix-prefetch-url --unpack <url>`
+#  sha256 = "06vy0qc4y3gsgm7s4d37aw4xfklmqvgp3mkpwvn9h2kbw19jh4pp";
+#}) {} }:
 
 let
   pkgs = [
