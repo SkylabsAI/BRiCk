d47f2f048881ade0bbbedd7f9a94d5b1b7991a5a
Gregory Malecha <gregory@bedrocksystems.com>
2022-11-03 21:47:24 -0400

    Remove debug printing.

--- src/ModuleBuilder.cpp
+++ src/ModuleBuilder.cpp
@@ -33,8 +33,10 @@ public:
     }
 
     void VisitDecl(const Decl *d, bool) {
+#if 0
         logging::debug() << "Unknown declaration kind \""
                          << d->getDeclKindName() << "\", dropping.\n";
+#endif
     }
 
     void VisitVarTemplateDecl(const VarTemplateDecl *decl, bool) {
