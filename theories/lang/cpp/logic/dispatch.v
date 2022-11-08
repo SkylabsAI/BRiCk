3b22b4e7ca6dc4ef2101daa45a50660194bd5b3c
Gregory Malecha <gregory@bedrocksystems.com>
2022-11-07 21:02:12 -0500

    Some fixes to virtual dispatch.

--- theories/lang/cpp/logic/dispatch.v
+++ theories/lang/cpp/logic/dispatch.v
@@ -48,15 +48,6 @@ Section with_cpp.
     | base :: rest => o_dot (o_base σ derived base) (derived_to_base base rest)
     end.
 
-  (** If successful, returns a pair of the function pointer to the
-   *  implementation and the downcast for [this] pointer. *)
-  Definition get_impl {σ} (base : globname) (path : list globname) (f : obj_name)
-    : option (ptr * globname * offset) :=
-    match dispatch.dispatch σ base path f with
-    | None => None
-    | Some (cls, path, fptr) => Some (_global fptr, cls, base_to_derived cls path)
-    end.
-
   Section tu.
     Context `{σ : genv}.
     Variable tu : translation_unit.
@@ -68,23 +59,6 @@ Section with_cpp.
       | None => None
       end.
 
-    Lemma tu_get_impl_ok (MOD : tu ⊧ σ) : forall base path fn a b c,
-        tu_get_impl base path fn = Some (a, b, c) ->
-        get_impl base path fn = Some (a, b, c).
-    Proof.
-      rewrite /tu_get_impl/get_impl; intros.
-      do 3 (case_match; try congruence).
-      erewrite tu_dispatch_ok; eauto.
-    Qed.
-
-    Lemma resolve_match_get_impl (MOD : tu ⊧ σ) {PROP : bi} base path fn a b c (P : _ -> PROP) :
-        tu_get_impl base path fn = Some (a, b, c) ->
-        P (a, b, c) -|-
-      match get_impl base path fn with
-      | Some x => P x
-      | None => False
-      end.
-    Proof. intros; erewrite tu_get_impl_ok; eauto. Qed.
   End tu.
 
   (** [resolve_virtual σ this cls f Q] returns [Q fa this'] if resolving [f] on
@@ -97,9 +71,9 @@ Section with_cpp.
              {σ : genv} (this : ptr) (cls : globname) (f : obj_name)
              (Q : forall (faddr : ptr) (cls_type : globname) (this_addr : ptr), mpred)
     : mpred :=
-    Exists (path : list globname), (* denoteModule (resolve:=σ) tu ** *)
+    Exists (path : list globname) tu, denoteModule tu **
       ((Exists q, this |-> identityR cls path q ** [| path <> nil |] ** True) //\\
-      match get_impl cls path f with
+      match tu_get_impl tu cls path f with
       | Some (fa, cls, off) => Q fa cls (_offset_ptr this off)
       | None => (* the function was not found or the implementation was pure virtual *)
         False
@@ -113,8 +87,8 @@ Section with_cpp.
     intros.
     rewrite /resolve_virtual.
     iIntros "X Y".
-    iDestruct "Y" as (path) "Y".
-    iExists path; iFrame.
+    iDestruct "Y" as (path tu) "[#mod Y]".
+    iExists path, tu; iFrame "∗#".
     iSplit.
     { iDestruct "Y" as "[$ _]". }
     { iDestruct "Y" as "[_ Y]". case_match => //.
