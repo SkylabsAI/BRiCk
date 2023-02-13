d70fd857b37f76d1c4aa2f2b5ad280968fbf544a
Yoichi Hirai <yoichi@bedrocksystems.com>
2023-02-13 20:39:50 +0100

    lens.v: add _nth lens

--- theories/prelude/lens.v
+++ theories/prelude/lens.v
@@ -5,7 +5,18 @@
  *)
 
 Require Export Lens.Lens.
+Require Import stdpp.list.
+Require Import bedrock.prelude.list_numbers.
 
 Definition _default {A B : Type} (default : B) : Lens.Lens A A B B :=
   {| Lens.view := fun _ => default
    ; Lens.over := fun _ x => x |}.
+
+Definition _nth {E} (n : N) : Lens.Lens (list E) (list E) (option E) E := {|
+  Lens.view := (fun l => l !! n) ;
+  Lens.over := (fun (f : option E -> E) (l : list E) =>
+    match l !! n with
+    | Some e => <[n := f (Some e)]> l
+    | None => l
+    end)
+|}.
