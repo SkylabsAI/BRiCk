c5ab83fa906ab3545079d70b5d48145cbb01eae5
Gregory Malecha <gregory@bedrocksystems.com>
2023-02-14 07:57:34 -0500

    Support for relational comparison operators on enumerations.
    - This is to support them on unscoped enumerations.

--- theories/lang/cpp/semantics/cast_operator.v
+++ theories/lang/cpp/semantics/cast_operator.v
@@ -52,21 +52,39 @@ Definition convert_type_op {σ : genv} (tu : translation_unit) (b : BinOp) (ty1 
   else if is_arithmetic ty1 && is_arithmetic ty2 then
     (* integer-integer operations *)
     match promote_integral tu ty1 , promote_integral tu ty2 with
-    | Some ty1 , Some ty2 =>
+    | Some ity1 , Some ity2 =>
       match b with
       | Bshl | Bshr =>
         (* heterogeneous operators *)
-        Some (ty1, ty2, ty1)
+        Some (ity1, ity2, ity1)
       | Badd | Bsub | Bmul | Bdiv | Bmod
-      | Band | Bor | Bxor
-      | Beq | Bneq
-      | Blt | Ble | Bgt | Bge
-      | Bcmp =>
+      | Band | Bor | Bxor =>
         (* homogeneous operators *)
-        match promote_arith ty1 ty2 with
+        match promote_arith ity1 ity2 with
         | Some to => Some (to, to, to)
         | _ => None
         end
+      | Beq | Bneq
+      | Blt | Ble | Bgt | Bge =>
+        let same_enum :=
+          match drop_qualifiers ty1 , drop_qualifiers ty2 with
+          | Tenum nm1 , Tenum nm2 => if bool_decide (nm1 = nm2) then true else false
+          | _ , _ => false
+          end
+        in
+        if same_enum then
+          (* Technically, this is only permitted for unscoped enumerations;
+             however, the dynamic semantics lines up with the comparison on
+             scoped enumerations so we do not test.
+           *)
+          Some (drop_qualifiers ty1, drop_qualifiers ty2, Tbool)
+        else
+          (* homogeneous operators *)
+          match promote_arith ity1 ity2 with
+          | Some to => Some (to, to, to)
+          | _ => None
+          end
+      | Bcmp => None
       | Bdotp
       | Bdotip
       | Bunsupported _ => None
