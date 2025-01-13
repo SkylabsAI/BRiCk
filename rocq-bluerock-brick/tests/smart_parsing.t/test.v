827f93aaf05e9397ddd1d186d49ff9b6692846b5
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-01-13 18:28:56 +0100

    Show that String Notations are import-bound and can be shadowed

--- rocq-bluerock-brick/tests/smart_parsing.t/test.v
+++ rocq-bluerock-brick/tests/smart_parsing.t/test.v
@@ -1,6 +1,8 @@
 Require Import bedrock.lang.cpp.syntax.
 Require test.test_cpp.
 
+Module notation_wrapper.
+
 (* TODO: this needs to be put in the [_cpp.v] file *)
 Definition parser x := parser.parse_name_with test_cpp.module (BS.parse x).
 Definition printer n := (match printer.print_name n with
@@ -9,10 +11,21 @@ Definition printer n := (match printer.print_name n with
                          end).
 String Notation core.name parser printer : cpp_name_scope.
 
-Goal "test(int, int)"%cpp_name = (Nglobal (Nfunction function_qualifiers.N (Nf "test") (Tint :: Tint :: nil))).
-Proof. reflexivity. Qed.
+End notation_wrapper.
+
+(* Global notation is used without [Import notation_wrapper.], type aliases don't work. *)
+Goal "test(INT, SINT)"%cpp_name = (Nglobal (Nfunction function_qualifiers.N (Nf "test") (Tint :: Tint :: nil))).
+Proof. Fail reflexivity. Abort.
+
+Import notation_wrapper.
+
+(* [Import notation_wrapper.] shadows notation and enables support for aliases. *)
 Goal "test(INT, SINT)"%cpp_name = (Nglobal (Nfunction function_qualifiers.N (Nf "test") (Tint :: Tint :: nil))).
 Proof. reflexivity. Qed.
+
+Goal "test(int, int)"%cpp_name = (Nglobal (Nfunction function_qualifiers.N (Nf "test") (Tint :: Tint :: nil))).
+Proof. reflexivity. Qed.
+
 Goal "test_e(enum E)"%cpp_name = (Nglobal (Nfunction function_qualifiers.N (Nf "test_e") (Tenum "E" :: nil))).
 Proof. reflexivity. Qed.
 
