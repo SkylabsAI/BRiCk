dbbfac43769945a22eccdb7f97b9859a799fa43d
Jan-Oliver Kaiser <janno@bluerock.io>
2025-03-03 13:24:13 +0100

    Implement our own version checking because that is a normal thing to do.

--- rocq-bluerock-cpp2v/cmake-helpers/clang-17/ClangHelperConfigVersion.cmake
+++ rocq-bluerock-cpp2v/cmake-helpers/clang-17/ClangHelperConfigVersion.cmake
@@ -1,2 +1,10 @@
 set(PACKAGE_VERSION "17.0")
 
+# LLVM is API-compatible only with matching major.minor versions
+# and patch versions not less than that requested.
+if("{PACKAGE_VERSION}" VERSION_EQUAL
+    "${PACKAGE_FIND_VERSION_MAJOR}.${PACKAGE_FIND_VERSION_MINOR}")
+  set(PACKAGE_VERSION_COMPATIBLE 1)
+endif()
+
+
