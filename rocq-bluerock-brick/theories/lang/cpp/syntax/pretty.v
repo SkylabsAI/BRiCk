78d145bf36a29072f690b54b4fcde1a41e49d06b
Gregory Malecha <gregory@bluerock.io>
2025-05-30 11:31:55 -0400

    WIP. Extend the pretty printer to support statements and expressions.
    All that remains is indentation.

--- rocq-bluerock-brick/theories/lang/cpp/syntax/pretty.v
+++ rocq-bluerock-brick/theories/lang/cpp/syntax/pretty.v
@@ -62,9 +62,21 @@ Section with_lang.
 
   Definition showN (n : N) : PrimString.string :=
     pstring.N.to_string n.
+  Definition showZ (z : Z) : PrimString.string :=
+    match z with
+    | Z0 => "0"
+    | Zpos p => showN (Npos p)
+    | Zneg p => "-" ++ showN (Npos p)
+    end.
 
   Definition parens (b : PrimString.string) : PrimString.string := "(" ++ b ++ ")".
   Definition angles (b : PrimString.string) : PrimString.string := "<" ++ b ++ ">".
+  Fixpoint sepBy (sep : PrimString.string) (ls : list PrimString.string) : PrimString.string :=
+    match ls with
+    | [] => ""
+    | l :: [] => l
+    | l :: ls => l ++ sep ++ sepBy sep ls
+    end.
 
   Section atomic_name.
     Context {type Expr : Set} (printType : type -> PrimString.string) (printExpr : Expr -> PrimString.string).
@@ -169,6 +181,91 @@ Section with_lang.
     | overloadable.Rsubscript => "[]"
     end.
 
+  Definition escape_char (n : N) : PrimString.string :=
+    match n with
+    | 0 => "\0"   (* Null *)
+    | 7 => "\a"   (* Alert/Bell *)
+    | 8 => "\b"   (* Backspace *)
+    | 9 => "\t"   (* Horizontal tab *)
+    | 10 => "\n"  (* Line feed/newline *)
+    | 11 => "\v"  (* Vertical tab *)
+    | 12 => "\f"  (* Form feed *)
+    | 13 => "\r"  (* Carriage return *)
+    | 34 => "\"""""  (* Double quote *)
+    | 39 => "\'"  (* Single quote *)
+    | 63 => "\?"  (* Question mark *)
+    | 92 => "\\"  (* Backslash *)
+    | n =>
+        (* For other characters, convert to ASCII if printable, or use hex escape for non-printable *)
+        if bool_decide (32 <= n <= 126) then
+          (* Printable ASCII range: convert to character *)
+          PrimString.make (Evaluate (Uint63Axioms.of_Z 1)) (Uint63Axioms.of_Z (Z.of_N n))
+        else
+          (* Non-printable: use hex escape sequence \xHH *)
+          let d1 := N.modulo (N.div n 16) 16 in
+          let d0 := N.modulo n 16 in
+          let hex_digit d :=
+            match d with
+            | 0 => "0"
+            | 1 => "1"
+            | 2 => "2"
+            | 3 => "3"
+            | 4 => "4"
+            | 5 => "5"
+            | 6 => "6"
+            | 7 => "7"
+            | 8 => "8"
+            | 9 => "9"
+            | 10 => "a"
+            | 11 => "b"
+            | 12 => "c"
+            | 13 => "d"
+            | 14 => "e"
+            | _ => "f"
+            end
+          in
+          "\x" ++ hex_digit d1 ++ hex_digit d0
+    end%N.
+
+  Definition literal_string_to_string (s : literal_string.t) : PrimString.string :=
+    let ps := literal_string.bytes s in
+    concat $ foldi (fun _ c cs => escape_char (Z.to_N (Uint63Axioms.to_Z (PrimString.get ps c))) :: cs) ps nil.
+
+  Definition printUO' (uo : UnOp) : PrimString.string :=
+    match uo with
+    | Uminus => "-"
+    | Uplus => "+"
+    | Unot => "!"
+    | Ubnot => "~"
+    | Uunsupported s => "/* " ++ s ++ " */"
+    end.
+  Definition printBO' (bo : BinOp) : PrimString.string :=
+    match bo with
+    | Badd => "+"
+    | Band => "&"
+    | Bcmp => "<=>"
+    | Bdiv => "/"
+    | Beq => "=="
+    | Bge => ">="
+    | Bgt => ">"
+    | Blt => "<"
+    | Ble => "<="
+    | Bmul => "*"
+    | Bneq => "!="
+    | Bor => "|"
+    | Bmod => "%"
+    | Bshl => "<<"
+    | Bshr => ">>"
+    | Bsub => "-"
+    | Bxor => "^"
+    | Bdotp => ".*"
+    | Bdotip => "->*"
+    | Bunsupported msg => "/* " ++ msg ++ " */"
+    end.
+
+  (* TODO: implement this *)
+  Definition indent (n : N) (p : PrimString.string) : PrimString.string := p.
+
   Fixpoint printN (nm : name) : PrimString.string :=
     match nm with
     | Nglobal an => printAN printT None an
@@ -245,10 +342,121 @@ Section with_lang.
     | Tresult_parenlist _ _
     | Tresult_member _ _ => "!nyi"
     end
+
   with printE (e : Expr) : PrimString.string :=
     match e with
     | Eglobal nm _ => printN nm
+    | Eparam id => id
+    | Eunresolved_global n => printN n
+    | Eunresolved_unop op e' =>
+        printUO op ++ " " ++ printE e'
+    | Eunresolved_binop op e1 e2 =>
+      printE e1 ++ " " ++ printBO op ++ " " ++ printE e2
+    | Eunresolved_call n args =>
+        printN n ++ parens (sepBy "," $ printE <$> args)
+    | Eunresolved_member_call n e' args => "nyi!"
+    | Eunresolved_parenlist opt_t args => "nyi!"
+    | Eunresolved_member e' n =>
+        printE e' ++ "." ++ printN n
+    | Evar ln t => ln
+
+    | Eenum_const n id =>
+        printN n ++ "::" ++ id
+    | Eglobal_member n t => "&" ++ printN n
+    | Echar num t =>
+        "'" ++ escape_char num ++ "'"
+    | Estring ls t =>
+        """" ++ literal_string_to_string ls ++ """"
+    | Eint val t => showZ val
+    | Ebool b =>
+        if b then "true" else "false"
+    | Eunop op e' t =>
+        printUO' op ++ printE e'
+    | Ebinop op e1 e2 t =>
+        printE e1 ++ " " ++ printBO' op ++ " " ++ printE e2
+    | Ederef e' t => "*" ++ printE e'
+    | Eaddrof e' => "&" ++ printE e'
+    | Eassign e1 e2 t => printE e1 ++ " = " ++ printE e2
+    | Eassign_op op e1 e2 t => printE e1 ++ " " ++ printBO' op ++ "= " ++ printE e2
+    | Epreinc e' t =>
+        "++" ++ printE e'
+    | Epostinc e' t =>
+        printE e' ++ "++"
+    | Epredec e' t =>
+        "--" ++ printE e'
+    | Epostdec e' t =>
+        printE e' ++ "--"
+    | Eseqand e1 e2 => printE e1 ++ " && " ++ printE e2
+    | Eseqor e1 e2 => printE e1 ++ " || " ++ printE e2
+    | Ecomma e1 e2 => printE e1 ++ ", " ++ printE e2
+
+    | Ecall e' args =>
+        printE e' ++ parens (sepBy "," $ printE <$> args)
+
+    | Eexplicit_cast style t e' =>
+        match style with
+        | cast_style.functional => printT t ++ "(" ++ printE e' ++ ")"
+        | cast_style.c => "(" ++ printT t ++ ")" ++ printE e'
+        | cast_style.static => "static_cast<" ++ printT t ++ ">(" ++ printE e' ++ ")"
+        | cast_style.const => "const_cast<" ++ printT t ++ ">(" ++ printE e' ++ ")"
+        | cast_style.dynamic => "dynamic_cast<" ++ printT t ++ ">(" ++ printE e' ++ ")"
+        | cast_style.reinterpret => "reinterpret_cast<" ++ printT t ++ ">(" ++ printE e' ++ ")"
+        end
+    | Ecast c e' => printE e' (* these casts are implicit *)
+    | Emember_ignore is_ptr e1 e2 =>
+        printE e1 ++ (if is_ptr then "->" else ".") ++ printE e2
+    | Emember_call is_ptr (inl (nm, _, _)) e' args =>
+        printE e' ++ (if is_ptr then "->" else ".") ++ printN nm ++ parens (sepBy ", " $ printE <$> args)
+    | Emember_call is_ptr (inr e) e' args =>
+        printE e' ++ (if is_ptr then "->*" else ".*") ++ printE e' ++ parens (sepBy ", " $ printE <$> args)
+    | Eoperator_call op _ [a] => printOO op ++ printE a
+    | Eoperator_call op _ [a;b] => printE a ++ " " ++ printOO op ++ " " ++ printE b
+    | Esubscript e1 e2 t =>
+        printE e1 ++ "[" ++ printE e2 ++ "]"
+    | Enull => "nullptr"
+    | Esizeof te t' =>
+        "sizeof(" ++ match te with
+          | inl t => printT t
+          | inr e => printE e
+          end ++ ")"
     | _ => "!nyi"
+    end
+
+  with printS (s : Stmt) : PrimString.string :=
+    match s with
+    | Sexpr e => printE e ++ ";"
+    | Sseq ss =>
+        "{
+" ++ indent 2 (sepBy "
+" $ printS <$> ss) ++ "
+}"
+    | Swhile None t s =>
+        "while (" ++ printE t ++ ") " ++ printS s
+    | Sdo (Sseq ss) t =>
+        "do {
+" ++ indent 2 (sepBy "
+" $ printS <$> ss) ++ "} while (" ++ printE t ++ ");"
+
+    | Sdecl ds =>
+        sepBy "
+" (printVD <$> ds)
+
+    | _ => "nyi!"
+    end
+
+  with printVD (v : VarDecl) : PrimString.string :=
+    let name_for bd :=
+      match bd with
+      | Bvar l _ _ => l
+      | Bbind l _ _ => l
+      end
+    in
+    match v with
+    | Dvar l t None => printT t ++ " " ++ l ++ ";"
+    | Dvar l t (Some e) => printT t ++ " " ++ l ++ " = " ++ printE e ++ ";"
+    | Ddecompose e _ bds => "auto [" ++ sepBy ", " (name_for <$> bds) ++ "] = " ++ printE e ++ ";"
+    | Dinit _ n t None => "static " ++ printT t ++ " " ++ printN n ++ ";"
+    | Dinit _ n t (Some e) => "static " ++ printT t ++ " " ++ printN n ++ " = " ++ printE e ++ ";"
     end.
 
 End with_lang.
@@ -266,3 +474,7 @@ Succeed Example _0 : TEST (Nglobal $ Nop function_qualifiers.N (OONew false) [])
 Succeed Example _0 : TEST (Nglobal $ Nop function_qualifiers.N (OODelete true) []) "operator delete[]()" := eq_refl.
 Succeed Example _0 : TEST (Nglobal $ Nop function_qualifiers.N (OODelete false) []) "operator delete()" := eq_refl.
 Succeed Example _0 : TEST (Nglobal $ Nop function_qualifiers.N (OOCoawait) []) "operator coawait()" := eq_refl.
+
+(* This seems to be the best we can do because we can not put a <<">> inside of a <<">> *)
+Succeed Example _0 : (printE $ Estring (literal_string.Build_t "hello world
+" 1) Tchar) = """hello world\n""" := eq_refl.
