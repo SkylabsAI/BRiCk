c16b93f5b204cc5b659fb36e02a20da90e436551
Jan-Oliver Kaiser <janno@bluerock.io>
2025-07-22 12:48:58 +0200

    Adapt to https://github.com/rocq-prover/rocq/pull/20427

--- ocaml-rocq-extra/src/extra_pp.ml
+++ ocaml-rocq-extra/src/extra_pp.ml
@@ -12,7 +12,8 @@ include Pp
 let rec no_box : t -> t = fun t ->
   match repr t with
   | Ppcmd_empty
-  | Ppcmd_string(_)        -> t
+  | Ppcmd_string(_)
+  | Ppcmd_sized_string(_)        -> t
   | Ppcmd_glue(ts)         -> unrepr (Ppcmd_glue(List.map no_box ts))
   | Ppcmd_box(_,t)
   | Ppcmd_tag(_,t)         -> no_box t
@@ -24,13 +25,14 @@ let to_flat_string : t -> string =
   let b = Buffer.create 1024 in
   let rec to_string t =
     match repr t with
-    | Ppcmd_empty            -> ()
-    | Ppcmd_string(s)        -> Buffer.add_string b s
-    | Ppcmd_glue(ts)         -> List.iter to_string ts
+    | Ppcmd_empty              -> ()
+    | Ppcmd_string(s)          -> Buffer.add_string b s
+    | Ppcmd_sized_string(_, s) -> Buffer.add_string b s (* TODO: handle size? *)
+    | Ppcmd_glue(ts)           -> List.iter to_string ts
     | Ppcmd_box(_,t)
-    | Ppcmd_tag(_,t)         -> to_string t
+    | Ppcmd_tag(_,t)           -> to_string t
     | Ppcmd_print_break(_,_)
-    | Ppcmd_force_newline    -> Buffer.add_char b ' '
-    | Ppcmd_comment(_)       -> ()
+    | Ppcmd_force_newline      -> Buffer.add_char b ' '
+    | Ppcmd_comment(_)         -> ()
   in
   fun t -> Buffer.clear b; to_string t; Buffer.contents b
